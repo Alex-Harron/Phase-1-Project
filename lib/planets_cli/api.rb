9200f0d5fff3e865fca27f58d1d8dd296aeb668d
Alex-Harron <74021683+Alex-Harron@users.noreply.github.com>
2021-01-10 13:34:16 -0500

    Update api.rb

--- lib/planets_cli/api.rb
+++ lib/planets_cli/api.rb
@@ -1,6 +1,6 @@
 class API
     def self.get_data
-        response = RestClient.get('https://swapi.dev/api/planets/')
+        response = RestClient.get('https://swapi.py4e.com/api/planets/')
         planets_array = JSON.parse(response)["results"]
         planets_array.each do |planet|
             Planet.new(planet)
